87014afa83820e1b3391ed2ff383f9b7f9e2e237
Joey Bruijns <bruijns.joey@gmail.com>
2020-01-11 16:43:03 +0100

    More small adjustments to gallery section

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 const galleryItems = document.getElementById('gallery');
+const card = document.getElementsByClassName('card');
 const peopleURL = 'https://randomuser.me/api/?results=12&seed=6dcfff5c6d6c8940';
 
+// Fetch the data and return 12 Objects for the employee directory
 async function fetchData(url) {
     const response = await fetch(url);
     console.log(response.url);
@@ -13,23 +15,30 @@ async function fetchData(url) {
     return Promise.all(profiles);
 }
 
+// Generate the HTML for the gallery section and fill it with the employee data
 function generateGalleryHTML(data) {
-    data.map(person => {
+    data.map(employee => {
         const div = document.createElement('div');
         div.setAttribute('class', 'card');
         galleryItems.appendChild(div);
         div.innerHTML = `
             <div class="card-img-container">
-                <img class="card-img" src="${person.picture.thumbnail}" alt="profile picture">
+                <img class="card-img" src="${employee.picture.thumbnail}" alt="profile picture">
             </div>
             <div class="card-info-container">
-                <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
-                <p class="card-text">${person.email}</p>
-                <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+                <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+                <p class="card-text">${employee.email}</p>
+                <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
             </div>
         `;
     });
 }
+
+// document.getElementsByClassName('card').addEventListener('click', (event) => {
+//     if (event.target.tagName === 'DIV') {
+//         alert('test123');
+//     }
+// });
 
 fetchData(peopleURL)
     .then(generateGalleryHTML);
